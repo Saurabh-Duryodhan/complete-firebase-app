f2eb7a12d99fe6c1ec88b94db3b978b290bded7f
Saurabh R. Duryodhan <srduryodhan97@gmail.com>
2022-08-08 19:08:39 +0530

    Firebase CRUD operations

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
 import React from "react";
 import { useState, useEffect } from "react";
-import { app } from "./firebaseConfig";
+import { app, database } from "./firebaseConfig";
+import {
+  collection,
+  addDoc,
+  getDocs,
+  doc,
+  updateDoc,
+  deleteDoc,
+} from "firebase/firestore";
 import {
   getAuth,
   createUserWithEmailAndPassword,
@@ -13,13 +21,24 @@ const App = () => {
   const auth = getAuth();
   const googleProvider = new GoogleAuthProvider();
   const gitProvider = new GithubAuthProvider();
+
+  const collectionRef = collection(database, "users");
+
   const [userInput, setUserInput] = useState("");
   const [data, setData] = useState();
+  const [allUsers, setALlUsers] = useState([]);
+  const [updateField, setUpdateField] = useState("");
 
   const handleInput = (e) => {
     const { name, value } = e.target;
     setUserInput({ ...userInput, [name]: value });
     setData({ ...data, ...userInput });
+  };
+
+  const handleUpdateField = (e) => {
+    const { name, value } = e.target;
+    setUpdateField({ ...updateField, [name]: value });
+    console.log(updateField);
   };
 
   const handleSignUp = async (e) => {
@@ -28,6 +47,17 @@ const App = () => {
     try {
       const user = await createUserWithEmailAndPassword(auth, email, password);
       console.log(user);
+      const saveUser = await addDoc(collectionRef, { ...data }).then(() => {
+        try {
+          signInWithEmailAndPassword(auth, email, password).then(() => {
+            console.log("user signed in");
+          });
+        } catch (error) {
+          console.log(error.message);
+        }
+        alert("User created successfully");
+      });
+      console.log(saveUser);
     } catch (err) {
       alert(err.message);
     }
@@ -39,6 +69,16 @@ const App = () => {
     try {
       const user = await signInWithEmailAndPassword(auth, email, password);
       console.log(user);
+    } catch (err) {
+      alert(err.message);
+    }
+  };
+
+  const addUserToDatabase = (e) => {
+    try {
+      addDoc(collectionRef, { ...data }).then(() =>
+        alert(`Data added to database`)
+      );
     } catch (err) {
       alert(err.message);
     }
@@ -60,6 +100,33 @@ const App = () => {
     } catch (error) {
       alert(error.message);
     }
+  };
+
+  const getUserData = async (e) => {
+    getDocs(collectionRef).then((response) => {
+      setALlUsers(
+        response.docs.map((item) => {
+          return { ...item.data(), id: item.id };
+        })
+      );
+    });
+  };
+
+  const updateEmail = (e) => {
+    e.preventDefault();
+    const { email } = updateField;
+    const docToBeUpdate = doc(collectionRef, "oWCophKjURlSmS3kYmjl");
+    updateDoc(docToBeUpdate, { email }).then(() => {
+      alert(`User updated`);
+    });
+  };
+
+  const dataDelete = (e) => {
+    e.preventDefault();
+    const docToBeDelete = doc(collectionRef, "oWCophKjURlSmS3kYmjl");
+    deleteDoc(docToBeDelete).then(() => {
+      alert(`User deleted`);
+    });
   };
 
   return (
@@ -94,7 +161,34 @@ const App = () => {
         <button type="submit" onClick={(e) => SignInWithGithub(e)}>
           GitHub SignIn
         </button>
+
+        <button type="submit" onClick={(e) => addUserToDatabase(e)}>
+          Save User
+        </button>
+
+        <button type="submit" onClick={(e) => getUserData(e)}>
+          Get All Users
+        </button>
+        <br />
+        <br />
+        <input
+          type="email"
+          placeholder="update your email"
+          name="email"
+          onChange={(e) => handleUpdateField(e)}
+        />
+        <button type="submit" onClick={(e) => updateEmail(e)}>
+          Update email
+        </button>
       </div>
+      {allUsers.map((item) => {
+        return (
+          <div key={item.key}>
+            <h2>{item.email}</h2>
+          </div>
+        );
+      })}
+      <button type="submit" onClick={(e)=> dataDelete(e)}>Delete User</button>
     </div>
   );
 };
--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,11 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCiM9z6pmZiiZWbI9SlYKsQE3NN_E-WUMk",
   authDomain: "firenestapp.firebaseapp.com",
@@ -16,5 +11,5 @@ const firebaseConfig = {
   measurementId: "G-GZKSBPSXJZ",
 };
 
-// Initialize Firebase
 export const app = initializeApp(firebaseConfig);
+export const database = getFirestore(app);
